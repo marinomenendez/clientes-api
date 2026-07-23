02c1523cb0b31fb41a7137bebbbc3d0d2d6052d9
marino <marino.menendez@gmail.com>
2026-07-23 12:29:31 +0200

    fase 4 implementación autenticación completada

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+export const authMiddleware = ( req, res, next ) => {
+
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res
+            .status(401)
+            .json({
+                message: 'Token requerido'
+            });
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+        //const decoded = jwt.verify( token, process.env.JWT_SECRET );
+        const decoded = jwt.verify( token, "Mundial2026" );
+        req.user = decoded;
+        next();
+    }
+    catch (error) {
+        return res
+            .status(401)
+            .json({
+                message: 'Token inválido'
+            });
+    }
+};
